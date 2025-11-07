7a34150da50c17478403085749732ebb92e571d3
和希鈴木 Kazuki Suzuki 🇯🇵🐱 かずきすずき <kazuki.suzuki.sadeghi@gmail.com>
2025-11-07 08:08:37 -0600

    Add files via upload

--- pyarchivefile.py
+++ pyarchivefile.py
@@ -647,7 +647,7 @@ __version_date_info__ = (2025, 11, 6, "RC 1", 1)
 __version_date__ = str(__version_date_info__[0]) + "." + str(
     __version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2)
 __revision__ = __version_info__[3]
-__revision_id__ = "$Id: ac98f2d8fa689bbcf7939f892030aad675a7d5fe $"
+__revision_id__ = "$Id$"
 if(__version_info__[4] is not None):
     __version_date_plusrc__ = __version_date__ + \
         "-" + str(__version_date_info__[4])
