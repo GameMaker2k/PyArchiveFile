df10c4aaf4c1e70889242ad6e5289a9f68c842a5
HimariKitsune <himarikitsune@gmail.com>
2026-05-23 19:12:46 -0500

    Initial commit

---
+++ pyarchivefile/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2018-2026 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2018-2026 Game Maker 2k - http://intdb.sourceforge.net/
+    Copyright 2018-2026 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: __init__.py - Last Update: 3/14/2026 Ver. 0.30.14 RC 1 - Author: cooldude2k $
+'''
+
+from .pyarchivefile import *
+from . import pyarchivefile
+
+__project__ = pyarchivefile.__project__
+__program_name__ = pyarchivefile.__program_name__
+__file_format_name__ = pyarchivefile.__file_format_name__
+__file_format_magic__ = pyarchivefile.__file_format_magic__
+__file_format_len__ = pyarchivefile.__file_format_len__
+__file_format_hex__ = pyarchivefile.__file_format_hex__
+__file_format_delimiter__ = pyarchivefile.__file_format_delimiter__
+__file_format_dict__ = pyarchivefile.__file_format_dict__
+__file_format_default__ = pyarchivefile.__file_format_default__
+__file_format_multi_dict__ = pyarchivefile.__file_format_multi_dict__
+__use_new_style__ = pyarchivefile.__use_new_style__
+__use_advanced_list__ = pyarchivefile.__use_advanced_list__
+__use_alt_inode__ = pyarchivefile.__use_alt_inode__
+__project_url__ = pyarchivefile.__project_url__
+__version_info__ = pyarchivefile.__version_info__
+__version_date_info__ = pyarchivefile.__version_date_info__
+__version_date__ = pyarchivefile.__version_date__
+__version_date_plusrc__ = pyarchivefile.__version_date_plusrc__
+__version__ = pyarchivefile.__version__
